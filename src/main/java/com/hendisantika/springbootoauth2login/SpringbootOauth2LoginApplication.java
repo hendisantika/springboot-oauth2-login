149bbd0f642c7b421c46592ef4d19189a4b448b1
hendisantika <hendisantika@yahoo.co.id>
2019-03-03 06:59:11 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootoauth2login/SpringbootOauth2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2LoginApplication.class, args);
+    }
+
+}
